90b8883b00d2051020155dfe123cb3aeaa2d8287
DeniSusanto <DeniSusanto@users.noreply.github.com>
2020-02-23 03:43:44 +0800

    Integration of facial features added

---
+++ integration/dark_eye_detection.py
@@ -0,0 +1,116 @@
+import cv2
+import pickle
+import numpy as np
+from skin_tone_estimator import get_skin_tone_RGB
+from sklearn.linear_model import LinearRegression
+import imutils
+import config
+
+D_E_WIDTH_RESCALE = config.D_E_WIDTH_RESCALE
+H_SPLIT_COEFF = config.H_SPLIT_COEFF
+W_SPLIT_COEFF = config.W_SPLIT_COEFF
+N_TOP_SELECT = config.N_TOP_SELECT
+D_E_MODEL_PATH = config.D_E_MODEL_PATH
+
+def split(x, n): 
+    split = []
+    if (x % n == 0): 
+        last = 0
+        for i in range(n): 
+            last += x//n
+            split.append(last)
+    else: 
+        zp = n - (x % n) 
+        pp = x//n
+        last = 0
+        for i in range(n): 
+            if(i>= zp):
+                last += pp + 1
+                split.append(last)
+            else:
+                last += pp
+                split.append(last) 
+    return split
+
+
+class DarkEyeDetector():
+    def __init__(self, facial_landmark):
+        self.facial_landmark = facial_landmark
+        self.image = facial_landmark.image
+        with open(D_E_MODEL_PATH, 'rb') as f:
+            self.score_model = pickle.load(f)
+    
+    #[[RGB_sum, r, g, b]]
+    def _custom_data_structure(self, img, y1, y2, x1, x2):
+        dat = []
+        crop = img[y1:y2, x1:x2]
+        RGB = get_skin_tone_RGB(crop)
+        totalRGB = np.sum(RGB)
+        dat.append(totalRGB)
+        for color in RGB:
+            dat.append(color)
+        return dat
+    
+    def _calculate_adjusted_dark_tone(self, img):
+        h = img.shape[0]
+        w = img.shape[1]
+        h_split = split(h, H_SPLIT_COEFF)
+        w_split = split(w, W_SPLIT_COEFF)
+        pix_ar = []
+        for i, _ in enumerate(h_split):
+            for j, _ in enumerate(w_split):
+                if i == 0:
+                    y1 = 0
+                else:
+                    y1 = h_split[i-1]
+                if j==0:
+                    x1 = 0
+                else:
+                    x1 = w_split[j-1]
+                y2 = h_split[i]
+                x2 = w_split[j]
+                pix_ar.append(self._custom_data_structure(img,y1,y2,x1,x2))
+        pix_ar.sort()
+        selected_patch = np.asarray(pix_ar[:N_TOP_SELECT])
+        return (int(round(np.mean(selected_patch[:,1]))), int(round(np.mean(selected_patch[:,2]))), int(round(np.mean(selected_patch[:,3]))))
+
+    def get_skin_tone(self):
+        #utilizes forehead region for getting overall face color
+        forehead_region = self.facial_landmark.get_forehead_region()
+        forehead_region = imutils.resize(forehead_region, width = D_E_WIDTH_RESCALE)
+        skin_tone = get_skin_tone_RGB(forehead_region)
+        return skin_tone
+    
+    #return right and left tone in RGB
+    def get_dark_eyes(self, darkest_only = False):
+        below_eyes_r, below_eyes_l = self.facial_landmark.get_below_eyes_region()
+        r_tone = self._calculate_adjusted_dark_tone(below_eyes_r)
+        l_tone = self._calculate_adjusted_dark_tone(below_eyes_l)
+        if darkest_only:
+            r_sum = np.sum(r_tone)
+            l_sum = np.sum(l_tone)
+            if r_sum < l_sum:
+                return r_tone
+            else:
+                return l_tone
+        return (r_tone, l_tone)
+    
+    def get_score(self):
+        face_tone = self.get_skin_tone()
+        eye_tone = self.get_dark_eyes(darkest_only = True)
+        dat = []
+        for color in face_tone:
+            dat.append(color)
+        for color in eye_tone:
+            dat.append(color)
+        for i in range(3):
+            diff = (face_tone[i] - eye_tone[i]) * 100 / face_tone[i]
+            dat.append(diff)
+        x = np.asarray([dat])
+        pred = self.score_model.predict(x)
+        pred[pred<0] = 0
+        score = pred[0]
+        return score
+        
+    
+    
---
+++ integration/facial_landmark.py
@@ -0,0 +1,164 @@
+__author__ = 'Deni Susanto'
+__copyright__ = 'Copyright 2020, AI Facial Skincare Advisor'
+__credits__ = ['Dr. Carmen L.', 'Dr. Adrian R.', 'Subhayan R.', 'Kevin H.', 'Mohammad A.']
+__email__ = 'densus@hku.hk'
+
+import dlib
+import imutils
+from imutils import face_utils
+import cv2
+import numpy as np
+import config
+
+F_L_PREDICTOR_PATH = config.F_L_PREDICTOR_PATH
+FL_WIDTH_RESIZE = config.FL_WIDTH_RESIZE
+
+class FacialLandmark():
+    def __init__(self, image):
+        detector = dlib.get_frontal_face_detector()
+        predictor = dlib.shape_predictor(F_L_PREDICTOR_PATH)
+        
+        (ori_height, ori_width) = image.shape[:2]
+        resized_image = imutils.resize(image, width=FL_WIDTH_RESIZE)
+        (trans_height, trans_width) = resized_image.shape[:2]
+        
+        ratio_x = ori_width/trans_width
+        ratio_y = ori_height/trans_height
+        
+        gray = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
+        rects = detector(gray, 1)
+        rect = rects[0]
+        shape = predictor(gray, rect)
+        landmark = face_utils.shape_to_np(shape)
+        adjusted_landmark = [(int(round(x * ratio_x)), int(round(y * ratio_y))) for (x,y) in landmark]
+        adjusted_landmark = np.asarray(adjusted_landmark)
+        (x, y, w, h) = face_utils.rect_to_bb(rect)
+        
+        self.image = image
+        self.resized_image = resized_image
+        self.landmark = adjusted_landmark
+        self.bb_x1 = int(round(x * ratio_x))
+        self.bb_y1 = int(round(y * ratio_y))
+        self.bb_x2 = int(round((x + w) * ratio_x))
+        self.bb_y2 = int(round((y + h) * ratio_y))
+    
+    def _returnImage(self, x1, x2, y1, y2, highLight):
+        if not highLight:
+            return self.image[y1:y2, x1:x2]
+        else:
+            img_copy = self.image.copy()
+            cv2.rectangle(img_copy, (x1, y1), (x2, y2), (0, 255, 0), 1)
+            return img_copy
+    
+    def get_below_eyes_region(self, highlight = False):
+        right_eye_h = round(1.2*(self.landmark[41][1] - self.landmark[37][1]))
+        left_eye_h = round(1.2*(self.landmark[46][1] - self.landmark[44][1]))
+        right_eye_lp = int(max(self.landmark[41][1], self.landmark[40][1]) + 0.5 * right_eye_h)
+        left_eye_lp = int(max(self.landmark[46][1], self.landmark[47][1]) + 0.5 * left_eye_h)
+        
+        r_x1 = self.landmark[36][0] - int(0.15 * (self.landmark[36][0] - self.landmark[0][0]))
+        r_x2 = self.landmark[39][0]
+        r_y1 = right_eye_lp
+        r_y2 = right_eye_lp + int(1.2 * right_eye_h)
+        
+        l_x1 = self.landmark[42][0]
+        l_x2 = self.landmark[45][0] + int(0.15 * (self.landmark[16][0] - self.landmark[45][0]))
+        l_y1 = left_eye_lp
+        l_y2 = left_eye_lp + int(1.2 * left_eye_h)
+        
+        return (self._returnImage(r_x1, r_x2, r_y1, r_y2, highlight), self._returnImage(l_x1, l_x2, l_y1, l_y2, highlight))
+    
+    def get_crows_feet_region(self, highlight = False):
+        right_cf_w = self.landmark[36][0] - self.landmark[0][0]
+        left_cf_w = self.landmark[16][0] - self.landmark[45][0]
+        right_cf_h = self.landmark[36][1] - self.landmark[17][1]
+        left_cf_h = self.landmark[45][1] - self.landmark[26][1]
+        r_w_portion = int(0.1 * right_cf_w)
+        l_w_portion = int(0.1 * left_cf_w)
+        r_h_portion = int(0.85 * right_cf_h)
+        l_h_portion = int(0.85 * left_cf_h)
+        r_x1 = self.landmark[0][0] + r_w_portion
+        r_x2 = self.landmark[36][0] - 3*r_w_portion
+        r_y1 = self.landmark[36][1] - r_h_portion
+        r_y2 = self.landmark[36][1] + round(1.5*r_h_portion)
+        
+        l_x1 = self.landmark[45][0] + 3*l_w_portion
+        l_x2 = self.landmark[16][0] - l_w_portion
+        l_y1 = self.landmark[45][1] - r_h_portion
+        l_y2 = self.landmark[45][1] + round(1.5*r_h_portion)
+        
+        return (self._returnImage(r_x1, r_x2, r_y1, r_y2, highlight), self._returnImage(l_x1, l_x2, l_y1, l_y2, highlight))
+    
+    def get_forehead_region(self, highlight = False):
+        face_y_min = int((min(self.landmark[:,1]).flatten()[0]))
+        face_y_max = int(np.asarray(max(self.landmark[:,1]).flatten()[0]))
+        face_height = face_y_max - face_y_min 
+        forehead_height = int(face_height * 0.25)
+        
+        x1 = self.landmark[37][0]
+        x2 = self.landmark[44][0]
+        y1 = face_y_min - forehead_height
+        y2 = face_y_min
+        
+        return self._returnImage(x1, x2, y1, y2, highlight)
+    
+    def get_nasal_junction_region(self, highlight = False):
+        r_lips = self.landmark[61][1] - self.landmark[50][1]
+        r_nose_alare = self.landmark[33][0] - self.landmark[31][0]
+        r_x1 = self.landmark[36][0]
+        r_x2 = self.landmark[31][0] - int(0.2 * r_nose_alare)
+        r_y1 = self.landmark[30][1]
+        r_y2 = self.landmark[48][1] - r_lips
+        
+        l_lips = self.landmark[63][1] - self.landmark[52][1]
+        l_nose_alare = self.landmark[35][0] - self.landmark[33][0]
+        l_x1 = self.landmark[35][0] + int(0.2 * l_nose_alare)
+        l_x2 = self.landmark[45][0]
+        l_y1 = self.landmark[30][1]
+        l_y2 = self.landmark[54][1] - l_lips
+        
+        return (self._returnImage(r_x1, r_x2, r_y1, r_y2, highlight), self._returnImage(l_x1, l_x2, l_y1, l_y2, highlight))
+    
+    def get_chin_region(self, highlight = False):
+        chin_x_min = self.landmark[39][0]
+        chin_x_max = self.landmark[42][0]
+        mouse_landmarks = self.landmark[list(range(48, 61)),:]
+        chin_y_min = np.max(np.array(mouse_landmarks[:,1]))
+        chin_y_max = int(np.asarray(max(self.landmark[:,1]).flatten()[0]))
+        
+        h = chin_y_max - chin_y_min
+        x1 = chin_x_min
+        x2 = chin_x_max
+        y1 = chin_y_min + int(0.05 * h)
+        y2 = chin_y_max - int(0.10 * h)
+        return self._returnImage(x1, x2, y1, y2, highlight)
+    
+    def get_cheeks_region(self, highlight = False):
+        face_x_min = int(max(0, np.asarray(min(self.landmark[:,0])).flatten()[0]))
+        face_x_max = int(np.asarray(max(self.landmark[:,0])).flatten()[0])
+        face_y_min = int((min(self.landmark[:,1]).flatten()[0]))
+        face_y_max = int(np.asarray(max(self.landmark[:,1]).flatten()[0]))
+        
+        r_cheek_min_x = int(face_x_min + 0.05 * (self.landmark[39][0] - face_x_min))
+        r_cheek_max_x = max(self.landmark[39][0], self.landmark[31][0])
+        l_cheek_min_x = min(self.landmark[42][0], self.landmark[35][0])
+        l_cheek_max_x = int(face_x_max - 0.05 * (face_x_max - self.landmark[42][0]))
+        
+        r_eye_landmarks = self.landmark[list(range(36, 42)),:]
+        l_eye_landmarks = self.landmark[list(range(42, 48)),:]
+        
+        r_cheek_min_y = int(max(r_eye_landmarks[:,1]) + 0.2 * (max(r_eye_landmarks[:,1]) - min(r_eye_landmarks[:,1])))
+        r_cheek_max_y = int(face_y_max - 0.1 * (face_y_max - max(r_eye_landmarks[:,1])))
+        l_cheek_min_y = int(max(l_eye_landmarks[:,1]) + 0.2 * (max(l_eye_landmarks[:,1])  - min(l_eye_landmarks[:,1])))
+        l_cheek_max_y = int(face_y_max - 0.1 * (face_y_max - max(l_eye_landmarks[:,1])))
+        
+        r_w = r_cheek_max_x - r_cheek_min_x
+        l_w = l_cheek_max_x - l_cheek_min_x
+        r_cheek_min_x = r_cheek_min_x + int(0.025 * r_w)
+        r_cheek_max_x = r_cheek_max_x - int(0.1 * r_w)
+        l_cheek_min_x = l_cheek_min_x + int(0.1 * l_w)
+        l_cheek_max_x = l_cheek_max_x - int(0.025 * l_w)
+        
+        return (self._returnImage(r_cheek_min_x, r_cheek_max_x, r_cheek_min_y, r_cheek_max_y, highlight), 
+                self._returnImage(l_cheek_min_x, l_cheek_max_x, l_cheek_min_y, l_cheek_max_y, highlight))
+        
